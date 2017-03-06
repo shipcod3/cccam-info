0fde602595ddf335986e6d3f4c888c8f45699152
Jay Turla <shipcodez@gmail.com>
2017-03-07 02:57:48 +0800

    Update cccaminfo.py

--- cccaminfo.py
+++ cccaminfo.py
@@ -28,10 +28,54 @@ def main(argv):
 
     host = sys.argv[1]
     command = sys.argv[2]
+    
+    #info command
     tn = telnetlib.Telnet(host, 16000)
     tn.read_until("Welcome to the CCcam information client.")
-    print "[+] Getting information on the receiver"
+    print "[+] Getting information on the receiver\n"
     tn.write("info\r")
+    print tn.read_all()
+    
+    #activeclients command
+    tn = telnetlib.Telnet(host, 16000)
+    tn.read_until("Welcome to the CCcam information client.")
+    print "[+] Active clients\n"
+    tn.write("info\r")
+    print tn.read_all()
+
+    #activeclients command
+    tn = telnetlib.Telnet(host, 16000)
+    tn.read_until("Welcome to the CCcam information client.")
+    print "[+] Active clients\n"
+    tn.write("info\r")
+    print tn.read_all()
+    
+    #clients command
+    tn = telnetlib.Telnet(host, 16000)
+    tn.read_until("Welcome to the CCcam information client.")
+    print "[+] Clients\n"
+    tn.write("clients\r")
+    print tn.read_all()
+    
+    #shares command
+    tn = telnetlib.Telnet(host, 16000)
+    tn.read_until("Welcome to the CCcam information client.")
+    print "[+] Active clients\n"
+    tn.write("shares\r")
+    print tn.read_all()
+
+    #providers command
+    tn = telnetlib.Telnet(host, 16000)
+    tn.read_until("Welcome to the CCcam information client.")
+    print "[+] Providers\n"
+    tn.write("providers\r")
+    print tn.read_all()
+
+    #entitlements command
+    tn = telnetlib.Telnet(host, 16000)
+    tn.read_until("Welcome to the CCcam information client.")
+    print "[+] Entitlements\n"
+    tn.write("entitlements\r")
     print tn.read_all()
 
 if __name__ == "__main__":
